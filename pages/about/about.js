f5e74fd8f17e16726a16f35693d28a14be58f92c
huanhuan789 <liuhuan.luck@qq.com>
2020-04-06 17:05:13 +0800

    注册小程序和页面

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,30 +1,30 @@
 // pages/about/about.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  //  页面的初始数据
   data: {
+list:[]
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+ // 生命周期函数--监听页面加载
   onLoad: function (options) {
-
+    // 请求数据
+wx.request({
+  url: 'http://123.207.32.32:8000/recommend',
+  success:(res)=>{
+console.log(res)
+const data=res.data.data;
+    console.log(this)
+this.setData({
+  list:data
+})
+  }
+})
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+// 生命周期函数--监听页面初次渲染完成
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+// 生命周期函数--监听页面显示
   onShow: function () {
 
   },
@@ -42,24 +42,22 @@ Page({
   onUnload: function () {
 
   },
-
+  // 监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
 
   },
-
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
   },
-
-  /**
-   * 用户点击右上角分享
-   */
+//  用户点击右上角分享
   onShareAppMessage: function () {
 
   }
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,44 @@
 // pages/home/home.js
+// 系统提供函数getApp()获取App()产生的示例对象
+const app=getApp()
+const name=app.globalData.name
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+name:'huanhuan',
+    students:[
+        {id:110,name:'hh',age:18},
+        {id:111,name:'jj',age:21},
+        {id:112,name:'aa',age:30},
+        {id:113,name:'qq',age:10}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+handbtnadd(){
+  // 这么写控制台有输出但页面不会刷新
+  // this.data.couter+=1
+  // console.log(this.data.couter)
+  // 需要调用setData函数
+  console.log(this)
+this.setData({
+ counter: this.data.counter +1
+})
+  console.log('点击了')
+  console.log(name)
+},
+  handbtncut(){
+    // 同理调用setData函数
+    this.setData({
+      counter:this.data.counter-1
+  
+    })
+    console.log('点击了')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取用户信息函数 小程序传进event对象
+  handleGetinfo(event){
+console.log(event)
   }
 })
